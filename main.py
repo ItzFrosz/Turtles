151d0919a861542055b450c29ca3af1ca1b6998d
ItzFrosz <shaposhnyk.artem@gmail.com>
2023-03-30 18:44:41 +0300

    Turtle Cursor left right forward

---
+++ main.py
@@ -0,0 +1,22 @@
+
+import turtle
+step = 50
+t = turtle.Turtle()
+t.forward(step)
+t.right(90)
+t.forward(step)
+t.left(90)
+t.forward(step)
+t.right(90)
+t.forward(step)
+t.left(90)
+t.forward(step)
+t.right(90)
+t.forward(step)
+t.right(90)
+t.forward(step)
+t.left(90)
+t.forward(step)
+
+
+turtle.done()
